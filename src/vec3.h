8fe6a4ccd9b6b5a6980a6c31bc86fd291164dede
Camille Vedani <camille.vedani@outlook.fr>
2025-07-16 15:59:07 +0200

    vec3 complété

--- src/vec3.h
+++ src/vec3.h
@@ -1,7 +1,11 @@
 class vec3 {
     public:
     vec3();  // constructeur par défaut
+
     vec3(double x_, double y_, double z_);
 
-    vec3 operator+(const vec3& v) const;
+    vec3 operator+(const vec3& v) const; //addition
+    vec3 operator-(const vec3& v) const; //soustraction
+    vec3 operator*(double t) const; //multiplication
+    double lenght() const; //norme du vecteur
 };
